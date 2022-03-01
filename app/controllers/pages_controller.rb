999cc3d161f34688591e3f6c7732c8b67ab3feed
sankar subramanian <tvl.shankar@gmail.com>
2022-03-01 17:20:59 +0100

    partial card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    #@user = User.all.filter { |user| user == current_user }
+    @user = User.all.sample
+    #@creations = Creation.all.filter { |creation| creation.user == current_user }
+    @creations = Creation.all.sample
+  end
 end
