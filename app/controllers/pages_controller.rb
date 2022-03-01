5ccebc520b1b5d94e1581d659a5e71a80fc29b3f
romain gille <gille.romain@yahoo.fr>
2022-03-01 17:09:46 +0100

    filtering buttons added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,18 @@ class PagesController < ApplicationController
   def home
   end
 
-  def index
+  def main
     @creations = Creation.all
     @events = Event.all
+    @categories = ["fab fa-canadian-maple-leaf", "fas fa-palette", "fas fa-cookie-bite", "fas fa-film",
+                   "fas fa-microchip", "fas fa-feather-alt", "fas fa-bars"]
   end
 end
+
+# Cooking
+# Collection
+# Cinematography
+# Garden
+# Technology (electric toy car, engineering projects, crazy scientific stuff)
+# Writing (storytelling, poetry)
+# Miscellaneous
