b5f925e0f2363b7f4faa6805fa289c794c5a9b52
Maximilian Scheider <maximilian-scheider@gmx.de>
2022-03-01 17:18:45 +0100

    remade design of homepage a bit, started to add stuff for the new creations

--- app/controllers/creations_controller.rb
+++ app/controllers/creations_controller.rb
@@ -1,2 +1,16 @@
 class CreationsController < ApplicationController
+  def new
+    @creation = Creation.new
+  end
+
+  def create
+    @creation = Creation.new(strong_params)
+    # @car.user = User.all.sample
+    @creation.user = current_user
+    if @creation.save
+      redirect_to creation_path(@creation)
+    else
+      render :new
+    end
+  end
 end
