6f2e8628dc45ba5a99fa06c573f26a8ea9cf6da4
romain gille <gille.romain@yahoo.fr>
2022-03-03 15:57:54 +0100

    stimulus done

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,26 @@
 class LikesController < ApplicationController
+
   def create
     @creation = Creation.find(params[:creation_id])
     @like = Like.new(creation: @creation, user: current_user)
     @like.save!
-
-    redirect_to creation_path(@creation)
+    respond_to do |format|
+      format.html { redirect_to creation_path(@creation) }
+      format.text do
+        render partial: "partials/like", locals: { my_creation: @creation, my_like: @like }, formats: [:html]
+      end
+    end
   end
 
   def destroy
     @like = Like.find(params[:id])
     @creation = @like.creation
     @like.destroy!
-
-    redirect_to creation_path(@creation)
+    respond_to do |format|
+      format.html { redirect_to creation_path(@creation) }
+      format.text do
+        render partial: "partials/like", locals: { my_creation: @creation, my_like: @like }, formats: [:html]
+      end
+    end
   end
 end
--- app/javascript/controllers/like_controller.js
+++ app/javascript/controllers/like_controller.js
@@ -1,16 +1,56 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["likebutton"];
+  static targets = ["likebutton", "textandbutton"];
 
   connect() {
     console.log("Hello from our first Stimulus controller");
     console.log(this.likebuttonTarget);
   }
 
-  togglelike(event) {
-    console.log("test");
-    // window.open(page);
+  addlike(event) {
     event.preventDefault();
+    event.stopImmediatePropagation();
+    const url = this.likebuttonTarget.href;
+    const csrfToken = document.head.querySelector("[name='csrf-token']");
+    console.log(csrfToken.content);
+    let headers = new window.Headers({
+      Accept: "text/plain",
+      "X-Requested-With": "XMLHttpRequest",
+    });
+    if (csrfToken) {
+      headers.append("X-CSRF-Token", csrfToken.content);
+    }
+    fetch(url, {
+      method: "post",
+      headers: headers,
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        this.textandbuttonTarget.outerHTML = data;
+      });
+  }
+
+  deletelike(event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    const url = this.likebuttonTarget.href;
+    const csrfToken = document.head.querySelector("[name='csrf-token']");
+    console.log(csrfToken.content);
+    let headers = new window.Headers({
+      Accept: "text/plain",
+      "X-Requested-With": "XMLHttpRequest",
+    });
+    if (csrfToken) {
+      headers.append("X-CSRF-Token", csrfToken.content);
+    }
+    fetch(url, {
+      method: "delete",
+      headers: headers,
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        this.textandbuttonTarget.outerHTML = data;
+      });
   }
 }
