ddd8186fcadd221b7be687f910fb348f24938eb0
sankar subramanian <tvl.shankar@gmail.com>
2022-03-04 12:57:44 +0100

    added index filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,17 +3,24 @@ class PagesController < ApplicationController
   end
 
   def main
-    @creations = Creation.all
-    @events = Event.all
     @categories = ["fab fa-canadian-maple-leaf", "fas fa-palette", "fas fa-cookie-bite", "fas fa-film",
-                   "fas fa-microchip", "fas fa-feather-alt", "fas fa-bars"]
+      "fas fa-microchip", "fas fa-feather-alt", "fas fa-bars"]
+    cat_name = ["art", "collections", "cooking", "photography", "technology", "garden", "misc"]
+    if params[:query].present?
+      index = @categories.index(params[:query])
+      @creations = Creation.where(category: cat_name[index])
+    else
+      @creations = Creation.all
+    end
+    @events = Event.all
+
   end
 end
 
-# Cooking
+# Art
 # Collection
+# Cooking
 # Cinematography
-# Garden
 # Technology (electric toy car, engineering projects, crazy scientific stuff)
-# Writing (storytelling, poetry)
+# Garden
 # Miscellaneous
