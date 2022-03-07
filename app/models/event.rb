acc32a9a0819fa3a113cc9341349326ae5b7d201
sankar subramanian <tvl.shankar@gmail.com>
2022-03-07 16:23:22 +0100

    aded geocoding and search function

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
   belongs_to :user
   has_many :participations, dependent: :destroy
   has_many :guests, through: :participations, source: :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :creations, dependent: :destroy
