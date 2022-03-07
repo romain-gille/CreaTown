a354ca49e4eb8cc5f8b5e59ffca4e79bf1462838
romain gille <gille.romain@yahoo.fr>
2022-03-07 17:13:51 +0100

    added category arrays in dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,18 +1,30 @@
 class DashboardController < ApplicationController
   before_action :set_user
   def mycreations
+    @categories = ["fas fa-paint-brush", "fas fa-utensils", "fas fa-camera-retro",
+                   "fas fa-stream", "fas fa-seedling"]
+    @cat_name = ["art", "cooking", "photography", "collections", "garden"]
     @creations = current_user.creations
   end
 
   def likedcreations
+    @categories = ["fas fa-paint-brush", "fas fa-utensils", "fas fa-camera-retro",
+                   "fas fa-stream", "fas fa-seedling"]
+    @cat_name = ["art", "cooking", "photography", "collections", "garden"]
     @creations_liked = current_user.liked_creations
   end
 
   def myevents
+    @categories = ["fas fa-paint-brush", "fas fa-utensils", "fas fa-camera-retro",
+                   "fas fa-stream", "fas fa-seedling"]
+    @cat_name = ["art", "cooking", "photography", "collections", "garden"]
     @events = current_user.events
   end
 
   def attendedevents
+    @categories = ["fas fa-paint-brush", "fas fa-utensils", "fas fa-camera-retro",
+                   "fas fa-stream", "fas fa-seedling"]
+    @cat_name = ["art", "cooking", "photography", "collections", "garden"]
     @events_attended = current_user.attended_events
   end
 
