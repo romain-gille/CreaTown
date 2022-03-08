9f3056988fa600d4109bd9cd42069a7cdc71e964
romain gille <gille.romain@yahoo.fr>
2022-03-08 12:07:03 +0100

    chat added and possibility to create new chat easily

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -9,4 +9,25 @@ class ChatroomsController < ApplicationController
       chat.user_connected_id == current_user.id || chat.user_messaged_id == current_user.id
     end
   end
+
+  def create
+    @chatroom = Chatroom.new(strong_params)
+
+    if @chatroom.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @chatroom = Chatroom.find(params[:id])
+    redirect_to chatroom_path(@chatroom)
+  end
+
+  private
+
+  def strong_params
+    params.require(:chatroom).permit(:name, :user_connected_id, :user_messaged_id)
+  end
 end
